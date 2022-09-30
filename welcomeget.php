e0e6712bc07e5c512ff9b00af2f88e95f22363ca
Denta Hidayat <denta.dent53@gmail.com>
2022-09-30 08:29:33 +0700

    adding index.php and create crud

--- welcomeget.php
+++ welcomeget.php
@@ -8,11 +8,8 @@
 </head>
 <body>
 
-<form action="welcomeget.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
 
 </body>
 </html>
